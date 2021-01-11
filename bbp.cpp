e6e2d6c123129b1c90fe7495e8c5ace4dbe050e0
Alexander Gomeniuk <gomeniuk.aa@gmail.com>
2021-01-11 22:05:08 +1000

    Minor platform tweaks

--- bbp.cpp
+++ bbp.cpp
@@ -1,21 +1,35 @@
+#include <iostream>
 #include <iomanip>
 #include <fstream>
+#include <chrono>
 
 // https://en.wikipedia.org/wiki/Bailey%E2%80%93Borwein%E2%80%93Plouffe_formula
 // do not confuse with
 // https://en.wikipedia.org/wiki/Spigot_algorithm
 
-template<int64_t base, int64_t exp>
+#ifdef __int128
+// Limit: 62
+static constexpr auto N = 62;
+static constexpr auto K = 31;
+using int_t = unsigned __int128;
+#else
+// Limit: 50
+static constexpr auto N = 50;
+static constexpr auto K = 15;
+using int_t = uint64_t;
+#endif
+
+template<int_t base, int_t exp>
 struct pow_ {
-	static constexpr int64_t value = base * pow_<base, exp - 1>::value;
+	static constexpr int_t value = base * pow_<base, exp - 1>::value;
 };
 
-template<int64_t base>
+template<int_t base>
 struct pow_<base, 0> {
-	static constexpr int64_t value = 1;
+	static constexpr int_t value = 1;
 };
 
-template<int64_t K>
+template<int_t K>
 struct pi {
 	static constexpr long double value =
 		(1.0 / pow_<16, K>::value) * (
@@ -26,14 +40,14 @@ struct pi {
 
 template<>
 struct pi<0> {
-	static constexpr long double value = 3.1333333333333;
+	static constexpr long double value = 4.0L - 0.5L - 1.0L / 5.0L - 1.0L / 6.0L;
 };
 
-// Limit: 50
-constexpr int64_t N = 50;
-
 int main() {
+	auto start = std::chrono::high_resolution_clock::now();
 	std::ofstream out("31415926.pi", std::ofstream::out);
-	out << std::setprecision(N + 1) << pi<15>::value << std::endl;
+	out << std::fixed << std::setprecision(N) << pi<K>::value << std::endl;
 	out.close();
+	auto stop = std::chrono::high_resolution_clock::now();
+	std::cout << "Run time elapsed: " << std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count() << " ms, haha templates are fun\n";
 }
